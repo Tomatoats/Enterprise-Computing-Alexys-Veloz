ba31bda33cbe8bf9511f3bcffcd502146ed41a24
Alexys Veloz <alexys.veloz.fl@gmail.com>
2023-10-06 01:22:10 -0400

    gonna start tonight

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+
+/*
+Name: Alexys Octavio Veloz
+Course: CNT 4714 Fall 2023
+Assignment title: Project 2 – Multi-threaded programming in Java
+Date: October 8, 2023
+Class: Main
+Description: a description of what the class provides would normally be expected.
+*/
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+}
